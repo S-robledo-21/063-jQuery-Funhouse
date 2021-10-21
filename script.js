ea2621f923cc6f8f25e8d604fba781a8152223ab
S-robledo-21 <77416831+S-robledo-21@users.noreply.github.com>
2021-10-21 17:27:01 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+$(".garage-door-opener").click(function() {
+      $(".garage-door").slideToggle();
+});
+
+$(" .pokeball  ").click(function() {
+      $(".pikachu").fadeToggle();
+});
+
+$(" .light-switch  ").click(function() {
+  $(".light-bulb").toggle();
+     $(".pikachu").toggle();
+});
+
+$(".football").click(function() {
+      $(".tackle").toggle();
+});
